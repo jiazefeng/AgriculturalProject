bb747eac5e673d6a9e7a5efbf4c219da0a93506b
jiazefeng <jiazefeng@maxrocky.com>
2016-07-07 18:29:46 +0800

    初始化项目

---
+++ adminweb/src/main/java/com/agricultural/common/result/ApiResult.java
@@ -0,0 +1,10 @@
+package com.agricultural.common.result;
+
+import org.springframework.ui.ModelMap;
+
+/**
+ * Created by Tom on 2016/5/9 17:25.
+ * Describe:
+ */
+public class ApiResult extends ModelMap {
+}
---
+++ webservice/src/main/java/com/agricultural/domain/demo/repository/DemoRepository.java
@@ -0,0 +1,51 @@
+package com.agricultural.domain.demo.repository;
+
+
+import com.agricultural.domain.demo.model.Demo;
+import com.agricultural.hibernate.BaseRepository;
+import com.agricultural.page.PageInfoTools;
+
+import java.util.List;
+
+/**
+ * Created by Tom on 2016/5/6 13:59.
+ * Describe:The demo repository.
+ */
+public interface DemoRepository extends BaseRepository<Demo> {
+
+
+    /**
+     * Describe:Return the Demo according to name.
+     * CreateBy:Tom
+     * CreateOn:2016-05-27 03:08:53
+     */
+    Demo getByName(String name);
+
+    /**
+     * Describe:Return the Demo List according to name.
+     * CreateBy:Tom
+     * CreateOn:2016-05-27 03:08:53
+     */
+    List<Demo> getList(String name);
+
+    /**
+     * Describe:Return count.
+     * CreateBy:Tom
+     * CreateOn:2016-05-31 12:10:17
+     */
+    int getCount();
+
+    /**
+     * Describe:Return the Demo page List.
+     * CreateBy:Tom
+     * CreateOn:2016-05-27 03:08:53
+     */
+    List<Demo> getList(PageInfoTools pageInfoTools);
+
+    /**
+     * Describe:Return the Demo List by hql.
+     * CreateBy:Tom
+     * CreateOn:2016-06-01 16:53:54
+     */
+    List<Object> getListByHql();
+}
