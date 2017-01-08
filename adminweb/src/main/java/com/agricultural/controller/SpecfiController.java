216211acf215303d94a3e260e4171b0e282f97d1
jiazefeng <905448771@qq.com>
2017-01-08 15:20:16 +0800

    增加规范管理

---
+++ adminweb/src/main/java/com/agricultural/controller/SpecfiController.java
@@ -0,0 +1,15 @@
+package com.agricultural.controller;
+
+import com.agricultural.service.specfi.inf.SpecfiService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Administrator on 2017/1/3 0003.
+ */
+@RestController
+@RequestMapping(value = "/specfi")
+public class SpecfiController {
+    //查询列表
+    
+}
